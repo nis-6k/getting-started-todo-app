427ee0527f91d4649fe7100305c407d3e401bc84
kumarnishant <niks_6@icloud.com>
2024-10-24 10:58:21 -0700

    chn

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -1,7 +1,11 @@
-const GREETING = 'Hello world!';
+const GREETING = [
+    "Wassup!",
+    "How are you!",
+    "How are you doing today!"
+];
 
 module.exports = async (req, res) => {
     res.send({
-        greeting: GREETING,
+        greeting: GREETING[Math.floor(Math.random()*GREETINGS.length)],
     });
 };
